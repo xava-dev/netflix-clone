f3dbc10849603707bd1b280b15b93a516dce6797
XAVA | Neill van Steenpaal <104568148+xava-dev@users.noreply.github.com>
2022-10-05 00:22:42 +0200

    Fixed prev and next elements in array

--- components/CardSection.tsx
+++ components/CardSection.tsx
@@ -5,18 +5,31 @@ import { useState } from "react";
 export default function CardSection(props: any) {
   const { title, videos, size } = props;
 
-  const [sliceA, setSliceA] = useState(0);
-  const [sliceB, setSliceB] = useState(6);
+  const [section, setSection] = useState(0);
 
-  const handleNext = () => {
-    setSliceA((sliceA) => sliceA + 6);
-    setSliceB((sliceB) => sliceB + 6);
-  };
+  function firstSection() {
+    let newVideos = videos.slice(0, 6);
+    console.log(newVideos);
+    return showNewVideos(newVideos);
+  }
 
-  const handleBack = () => {
-    setSliceA((sliceA) => sliceA - 6);
-    setSliceB((sliceB) => sliceB - 6);
-  };
+  function secondSection() {
+    let newVideos = videos.slice(6, 12);
+    console.log(newVideos);
+    return showNewVideos(newVideos);
+  }
+
+  function thirdSection() {
+    let newVideos = videos.slice(12, 18);
+    console.log(newVideos);
+    return showNewVideos(newVideos);
+  }
+
+  function showNewVideos(newVideos: any) {
+    return newVideos.map((video: { imgUrl: string }, index: string) => {
+      return <Card key={index} id={index} imgUrl={video.imgUrl} size={size} />;
+    });
+  }
 
   return (
     <div className="relative mx-3 lg:mx-14 mb-12 z-30">
@@ -24,24 +37,18 @@ export default function CardSection(props: any) {
         <h2 className="text-md lg:text-2xl text-[#e5e5e5] font-medium">
           {title}
         </h2>
-        <p className="text-[#e5e5e5] ml-auto mr-0">
-          {sliceA} - {sliceB}
-        </p>
+        <p className="text-[#e5e5e5] ml-auto mr-0"></p>
       </div>
       <IoIosArrowBack
-        onClick={handleBack}
+        onClick={() => setSection(section - 1)}
         className={`${
-          sliceA === 0 ? "hidden" : "block"
+          section === 0 ? "hidden" : "block"
         } text-white text-4xl -mb-36 -translate-x-10 relative z-10 hover:cursor-pointer rounded h-36`}
       />
       <div className="hidden xl:grid grid-flow-col grid-rows-1 gap-2">
-        {videos
-          .slice(sliceA, sliceB)
-          .map((video: { imgUrl: string }, index: string) => {
-            return (
-              <Card key={index} id={index} imgUrl={video.imgUrl} size={size} />
-            );
-          })}
+        {section === 0 ? firstSection() : ""}
+        {section === 1 ? secondSection() : ""}
+        {section === 2 ? thirdSection() : ""}
       </div>
       <div className="grid xl:hidden grid-flow-col grid-rows-1 gap-2">
         {videos.slice(0, 4).map((video: { imgUrl: string }, index: string) => {
@@ -51,9 +58,9 @@ export default function CardSection(props: any) {
         })}
       </div>
       <IoIosArrowForward
-        onClick={handleNext}
+        onClick={() => setSection(section + 1)}
         className={`${
-          sliceB === 18 ? "hidden" : "block"
+          section === 2 ? "hidden" : "block"
         } ml-auto mr-0 text-white text-4xl -mb-36 -translate-y-36 translate-x-10 relative z-10 hover:cursor-pointer rounded h-36`}
       />
     </div>
